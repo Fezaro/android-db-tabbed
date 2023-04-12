aca6b8b12593f616c489b0d6648b95c4a1a7b1ae
Fezaro <mxmugendi@gmail.com>
2023-04-12 18:09:05 +0300

    add gender spinner

--- app/src/main/java/com/example/tabbedviewtut/fragments/DetailsFragment.java
+++ app/src/main/java/com/example/tabbedviewtut/fragments/DetailsFragment.java
@@ -8,8 +8,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.tabbedviewtut.DbHelper;
@@ -21,6 +24,7 @@ public class DetailsFragment extends Fragment {
 
     EditText firstName,lastName,idNumber,courseName,gender;
     Button button, updateButton, deleteButton, adminButton;
+    Spinner g_spinner;
     DbHelper DB;
 
     @Override
@@ -39,11 +43,33 @@ public class DetailsFragment extends Fragment {
         lastName = view.findViewById(R.id.lastName);
         idNumber = view.findViewById(R.id.idNumber);
         courseName = view.findViewById(R.id.courseName);
-        gender = view.findViewById(R.id.gender);
+//        gender = view.findViewById(R.id.gender);
         button = view.findViewById(R.id.button);
         updateButton = view.findViewById(R.id.updateButton);
         deleteButton = view.findViewById(R.id.deleteButton);
         adminButton = view.findViewById(R.id.adminButton);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        String[] genders = {"Male", "Female", "Other"};
+        ArrayAdapter<String> genderAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_dropdown_item, genders);
+        g_spinner = view.findViewById(R.id.gender_spinner);
+        g_spinner.setAdapter(genderAdapter);
+
+        g_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedGender = (String) adapterView.getItemAtPosition(i);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+
 
         // set button click listener
         button.setOnClickListener(new View.OnClickListener() {
@@ -117,7 +143,8 @@ public class DetailsFragment extends Fragment {
         studentDetails[1] = lastName.getText().toString();
         studentDetails[2] = idNumber.getText().toString();
         studentDetails[3] = courseName.getText().toString();
-        studentDetails[4] = gender.getText().toString();
+        studentDetails[4] = g_spinner.getSelectedItem().toString();
+
         return studentDetails;
     }
 
@@ -142,7 +169,7 @@ public class DetailsFragment extends Fragment {
         firstName.getText().clear();
         lastName.getText().clear();
         idNumber.getText().clear();
-        gender.getText().clear();
+//        gender.getText().clear();
         courseName.getText().clear();
         
     }
